33ece1959aa7cccc5993ee669ae0791ef24a55b3
Esteban <juradoesteban001@gmail.com>
2026-03-11 10:25:31 -0500

    crea componente navbar

---
+++ portafolio/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>Esteban.dev</h2>
+      <ul>
+        <li>Inicio</li>
+        <li>Sobre mi</li>
+        <li>Skills</li>
+        <li>Proyectos</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
